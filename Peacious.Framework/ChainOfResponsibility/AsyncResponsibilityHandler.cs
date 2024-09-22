d83d4a2809109c83f7024fc8d4f09a5bd6c08846
Khairul-Anam-Mubin <anam.mubin1999@gmail.com>
2024-09-22 22:48:11 +0600

    generic chain of responsibility implementation added

---
+++ Peacious.Framework/ChainOfResponsibility/AsyncResponsibilityHandler.cs
@@ -0,0 +1,37 @@
+﻿namespace Peacious.Framework.ChainOfResponsibility;
+
+public interface IAsyncResponsibilityHandler<TRequest, TResponse>
+{
+    Task<TResponse> HandleAsync(TRequest request);
+}
+
+public abstract class AAsyncResponsibilityHandler<TRequest, TResponse>
+    : IAsyncResponsibilityHandler<TRequest, TResponse>
+{
+    private IAsyncResponsibilityHandler<TRequest, TResponse>? NextHandler { get; set; }
+
+    protected abstract Task<bool> CanHandleAsync(TRequest request);
+    protected abstract Task<TResponse> OnHandleAsync(TRequest request);
+
+    public IAsyncResponsibilityHandler<TRequest, TResponse> SetNext(
+        IAsyncResponsibilityHandler<TRequest, TResponse> nextHandler)
+    {
+        NextHandler = nextHandler;
+        return this;
+    }
+
+    public async Task<TResponse> HandleAsync(TRequest request)
+    {
+        if (await CanHandleAsync(request))
+        {
+            return await OnHandleAsync(request);
+        }
+
+        if (NextHandler is not null)
+        {
+            return await NextHandler.HandleAsync(request);
+        }
+
+        throw new Exception("No handler satisfied to handle the request");
+    }
+}
---
+++ Peacious.Framework/ChainOfResponsibility/ResponsibilityHandler.cs
@@ -0,0 +1,37 @@
+﻿namespace Peacious.Framework.ChainOfResponsibility;
+
+public interface IResponsibilityHandler<TRequest, TResponse>
+{
+    TResponse Handle(TRequest request);
+}
+
+public abstract class AResponsibilityHandler<TRequest, TResponse>
+    : IResponsibilityHandler<TRequest, TResponse>
+{
+    private IResponsibilityHandler<TRequest, TResponse>? NextHandler { get; set; }
+
+    protected abstract bool CanHandle(TRequest request);
+    protected abstract TResponse OnHandle(TRequest request);
+
+    public IResponsibilityHandler<TRequest, TResponse> SetNext(
+        IResponsibilityHandler<TRequest, TResponse> nextHandler)
+    {
+        NextHandler = nextHandler;
+        return this;
+    }
+
+    public TResponse Handle(TRequest request)
+    {
+        if (CanHandle(request))
+        {
+            return OnHandle(request);
+        }
+
+        if (NextHandler is not null)
+        {
+            return NextHandler.Handle(request);
+        }
+
+        throw new Exception("No handler satisfied to handle the request");
+    }
+}
